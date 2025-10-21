351d09d5a8406bf5bbd83d61f5d8769f222c2625
alban <alban.flouvat@epfedu.fr>
2025-10-21 16:15:32 +0200

    initial commit

---
+++ TPDocker/docker-entrypoint-initdb.d/02-InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO departments (name) VALUES ('IRC'),('ETI'),('CGP');
+
+INSERT INTO students (department_id, first_name, last_name) VALUES
+ (1,'Eli','Copter'),
+ (2,'Emma','Carena'),
+ (2,'Jack','Uzzi'),
+ (3,'Aude','Javel');
